8b6f7a740c598b4cc0ebec265dffc2d9bf997c6e
niklasindorf <niklas.indorf@outlook.de>
2024-09-14 11:11:35 +0200

    Added test for compute_covering_relation.

--- fcatng/algorithms/tests/test_norris.py
+++ fcatng/algorithms/tests/test_norris.py
@@ -6,7 +6,7 @@ import pytest
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..')))  # Includes the above folder
 from fcatng.algorithms import norris, derivation
-from fcatng import context
+from fcatng import context, Concept, ConceptSystem, compute_covering_relation
 from fcatng.tests import helper_test
 
 
@@ -37,7 +37,6 @@ def test_norris(test_data):
     result_list_ext = []
 
     for element in norris_data[0]:
-        print(element.extent)
         element_list_objs.append(element.intent)
         element_list_attrs.append(element.extent)
 
@@ -55,3 +54,27 @@ def test_norris(test_data):
 
     assert element_list_attrs == result_list_ext
     assert element_list_objs == result_list_int
+
+
+@pytest.mark.parametrize("test_data", helper_test.get_test_data(file_path))
+def test_compute_covering_relation(test_data):
+    global key_int, key_ext, val_cov
+    concept = Concept(test_data["concept"][0], test_data["concept"][1])
+    conc_sys = ConceptSystem([concept])
+
+    covering_relation = compute_covering_relation(conc_sys)
+
+    for key, val in covering_relation.items():
+        key_ext = key.extent
+        key_int = key.intent
+        val_cov = val
+
+    if test_data["concept_val"][0] == []:
+        correct_result = set()
+    else:
+        correct_result = test_data["concept_val"]
+
+    assert key_ext == set(test_data["concept"][0])
+    assert key_int == set(test_data["concept"][1])
+    assert val_cov == correct_result
+
